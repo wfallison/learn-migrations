3b02d71257c6dd32edcef794618eae4ad2a1d474
Billy Allison <williamfallison@gmail.com>
2023-02-25 00:24:06 -0800

    Adds view test2

--- data/migrations/20230224214524_todo.js
+++ data/migrations/20230224214524_todo.js
@@ -6,6 +6,15 @@ exports.up = knex =>
   knex.schema.createTable("todo", tbl => {
     tbl.increments();
     tbl.text("task", 128).notNullable();
+
+    knex.schema.raw(`
+      CREATE OR REPLACE VIEW public.test2
+      AS
+      SELECT todo.id,
+        todo.task
+        FROM todo;
+    `);
+
   });
 
 /**
